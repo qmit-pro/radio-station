f3f94881b4820add1ec0bc95bc5badd44b98c9cc
이영주 <104013908+hmds1606@users.noreply.github.com>
2022-10-26 10:01:30 +0900

    feat: add eslint prettier (#7)
    * feat: add eslint prettier
    
    * style: eslint 오류 수정

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/no-throw-literal": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/promise-function-async": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/dot-notation": "off"
+  },
+};
