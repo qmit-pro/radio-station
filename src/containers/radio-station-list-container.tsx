c70c9c2c903f615a3c2a968e5c7da27026c8fd04
hmds1606 <hmds1606@gmail.com>
2022-11-01 15:59:16 +0900

    feat: fix apis

--- src/containers/radio-station-list-container.tsx
+++ src/containers/radio-station-list-container.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import RadioStationList, {
   RadioList,
 } from "../components/list/radio-station-list";
+import { getListApi } from "../apis/apis";
 
 export default function RadioStationListContainer() {
   const navigation = useNavigate();
@@ -16,9 +17,7 @@ export default function RadioStationListContainer() {
   };
 
   useEffect(() => {
-    fetch("http://all.api.radio-browser.info/json/stations/byname/pop?limit=20")
-      .then((res) => res.json())
-      .then(setRadioList);
+    getListApi().then(setRadioList);
   }, []);
 
   return (
